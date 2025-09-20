e63b2a09717a0af6c92b60edfdaeeac77cce5fc8
namanparikh <40051160+namanparikh@users.noreply.github.com>
2025-09-20 16:10:52 +0530

    Update google_form_resume_matcher.py
    Signed-off-by: namanparikh <40051160+namanparikh@users.noreply.github.com>

--- google_form_resume_matcher.py
+++ google_form_resume_matcher.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import warnings
 import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 import tempfile
 import os
 from fuzzywuzzy import fuzz
